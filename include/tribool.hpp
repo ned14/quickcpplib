b9102c0cabda43d46054bbbc9e82cb138ad04fb5
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-06-24 09:14:36 +0100

    Moved tribool into namespace boost-lite.

--- include/tribool.hpp
+++ include/tribool.hpp
@@ -38,71 +38,74 @@ DEALINGS IN THE SOFTWARE.
 
 #include <istream>
 
-namespace tribool
+namespace boost_lite
 {
-  /*! \defgroup tribool Constexpr C++ 11 tribool
-
-  This tries to be mostly compatible with Boost.Tribool, except it's written in C++ 11 and is 100%
-  constexpr throughout by being a strongly typed enum. Note that `other` state is aliased to
-  `indeterminate` for Boost.Tribool compatibility. It is also aliased to unknown.
-
-  Unlike Boost.Tribool this deliberately does not provide automatic conversion to bool. It was
-  always questionable if code coped well with `operator!` != `!operator bool` anyway. Use the
-  free functions true_(), false_(), other(), indeterminate() and unknown() to test the contents
-  of this tribool.
-
-  For sorting, false < unknown < true. Same for max/min. STL functions operators << and >>
-  for iostream are defined.
-  */
-  //! \brief A constexpr C++ 11 tribool \ingroup tribool
-  enum class tribool : signed char
+  namespace tribool
   {
-    false_ = -1,        //!< False
-    true_ = 1,          //!< True
-    other = 0,          //!< Other/Indeterminate/Unknown
-    indeterminate = 0,  //!< Other/Indeterminate/Unknown
-    unknown = 0         //!< Other/Indeterminate/Unknown
-  };
-  //! \brief Explicit construction from some signed integer. <0 false, >0 true, 0 is other \ingroup tribool
-  constexpr inline tribool make_tribool(int v) noexcept { return v > 0 ? tribool::true_ : v < 0 ? tribool::false_ : tribool::other; }
-  //! \brief If tribool is true return false, if tribool is false return true, else return other \ingroup tribool
-  constexpr inline tribool operator~(tribool v) noexcept { return static_cast<tribool>(-static_cast<signed char>(v)); }
-  //! \brief If a is true and b is true, return true, if either is false return false, else return other \ingroup tribool
-  constexpr inline tribool operator&(tribool a, tribool b) noexcept { return (a == tribool::true_ && b == tribool::true_) ? tribool::true_ : (a == tribool::false_ || b == tribool::false_) ? tribool::false_ : tribool::other; }
-  //! \brief If a is true or b is true, return true, if either is other return other, else return false \ingroup tribool
-  constexpr inline tribool operator|(tribool a, tribool b) noexcept { return (a == tribool::true_ || b == tribool::true_) ? tribool::true_ : (a == tribool::other || b == tribool::other) ? tribool::other : tribool::false_; }
+    /*! \defgroup tribool Constexpr C++ 11 tribool
 
-  //  //! \brief If tribool is false return true, else return false \ingroup tribool
-  //  constexpr inline bool operator !(tribool v) noexcept { return a==tribool::false_; }
-  //! \brief If a is true and b is true, return true \ingroup tribool
-  constexpr inline bool operator&&(tribool a, tribool b) noexcept { return (a == tribool::true_ && b == tribool::true_); }
-  //! \brief If a is true or b is true, return true \ingroup tribool
-  constexpr inline bool operator||(tribool a, tribool b) noexcept { return (a == tribool::true_ || b == tribool::true_); }
+    This tries to be mostly compatible with Boost.Tribool, except it's written in C++ 11 and is 100%
+    constexpr throughout by being a strongly typed enum. Note that `other` state is aliased to
+    `indeterminate` for Boost.Tribool compatibility. It is also aliased to unknown.
 
-  //! \brief Return true if tribool is true. \ingroup tribool
-  constexpr inline bool true_(tribool a) noexcept { return a == tribool::true_; }
-  //! \brief Return true if tribool is true. \ingroup tribool
-  constexpr inline bool false_(tribool a) noexcept { return a == tribool::false_; }
-  //! \brief Return true if tribool is other/indeterminate/unknown. \ingroup tribool
-  constexpr inline bool other(tribool a) noexcept { return a == tribool::indeterminate; }
-  //! \brief Return true if tribool is other/indeterminate/unknown. \ingroup tribool
-  constexpr inline bool indeterminate(tribool a) noexcept { return a == tribool::indeterminate; }
-  //! \brief Return true if tribool is other/indeterminate/unknown. \ingroup tribool
-  constexpr inline bool unknown(tribool a) noexcept { return a == tribool::indeterminate; }
+    Unlike Boost.Tribool this deliberately does not provide automatic conversion to bool. It was
+    always questionable if code coped well with `operator!` != `!operator bool` anyway. Use the
+    free functions true_(), false_(), other(), indeterminate() and unknown() to test the contents
+    of this tribool.
+
+    For sorting, false < unknown < true. Same for max/min. STL functions operators << and >>
+    for iostream are defined.
+    */
+    //! \brief A constexpr C++ 11 tribool \ingroup tribool
+    enum class tribool : signed char
+    {
+      false_ = -1,        //!< False
+      true_ = 1,          //!< True
+      other = 0,          //!< Other/Indeterminate/Unknown
+      indeterminate = 0,  //!< Other/Indeterminate/Unknown
+      unknown = 0         //!< Other/Indeterminate/Unknown
+    };
+    //! \brief Explicit construction from some signed integer. <0 false, >0 true, 0 is other \ingroup tribool
+    constexpr inline tribool make_tribool(int v) noexcept { return v > 0 ? tribool::true_ : v < 0 ? tribool::false_ : tribool::other; }
+    //! \brief If tribool is true return false, if tribool is false return true, else return other \ingroup tribool
+    constexpr inline tribool operator~(tribool v) noexcept { return static_cast<tribool>(-static_cast<signed char>(v)); }
+    //! \brief If a is true and b is true, return true, if either is false return false, else return other \ingroup tribool
+    constexpr inline tribool operator&(tribool a, tribool b) noexcept { return (a == tribool::true_ && b == tribool::true_) ? tribool::true_ : (a == tribool::false_ || b == tribool::false_) ? tribool::false_ : tribool::other; }
+    //! \brief If a is true or b is true, return true, if either is other return other, else return false \ingroup tribool
+    constexpr inline tribool operator|(tribool a, tribool b) noexcept { return (a == tribool::true_ || b == tribool::true_) ? tribool::true_ : (a == tribool::other || b == tribool::other) ? tribool::other : tribool::false_; }
+
+    //  //! \brief If tribool is false return true, else return false \ingroup tribool
+    //  constexpr inline bool operator !(tribool v) noexcept { return a==tribool::false_; }
+    //! \brief If a is true and b is true, return true \ingroup tribool
+    constexpr inline bool operator&&(tribool a, tribool b) noexcept { return (a == tribool::true_ && b == tribool::true_); }
+    //! \brief If a is true or b is true, return true \ingroup tribool
+    constexpr inline bool operator||(tribool a, tribool b) noexcept { return (a == tribool::true_ || b == tribool::true_); }
+
+    //! \brief Return true if tribool is true. \ingroup tribool
+    constexpr inline bool true_(tribool a) noexcept { return a == tribool::true_; }
+    //! \brief Return true if tribool is true. \ingroup tribool
+    constexpr inline bool false_(tribool a) noexcept { return a == tribool::false_; }
+    //! \brief Return true if tribool is other/indeterminate/unknown. \ingroup tribool
+    constexpr inline bool other(tribool a) noexcept { return a == tribool::indeterminate; }
+    //! \brief Return true if tribool is other/indeterminate/unknown. \ingroup tribool
+    constexpr inline bool indeterminate(tribool a) noexcept { return a == tribool::indeterminate; }
+    //! \brief Return true if tribool is other/indeterminate/unknown. \ingroup tribool
+    constexpr inline bool unknown(tribool a) noexcept { return a == tribool::indeterminate; }
+  }
 }
 
 namespace std
 {
-  inline istream &operator>>(istream &s, tribool::tribool &a)
+  inline istream &operator>>(istream &s, boost_lite::tribool::tribool &a)
   {
     char c;
     s >> c;
-    a = (c == '1') ? tribool::tribool::true_ : (c == '0') ? tribool::tribool::false_ : tribool::tribool::other;
+    a = (c == '1') ? boost_lite::tribool::tribool::true_ : (c == '0') ? boost_lite::tribool::tribool::false_ : boost_lite::tribool::tribool::other;
     return s;
   }
-  inline ostream &operator<<(ostream &s, tribool::tribool a)
+  inline ostream &operator<<(ostream &s, boost_lite::tribool::tribool a)
   {
-    char c = (a == tribool::tribool::true_) ? '1' : (a == tribool::tribool::false_) ? '0' : '?';
+    char c = (a == boost_lite::tribool::tribool::true_) ? '1' : (a == boost_lite::tribool::tribool::false_) ? '0' : '?';
     return s << c;
   }
 }
