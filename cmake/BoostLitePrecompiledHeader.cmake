35e22292c82734e73e49b6bd370ec63c74ce2e35
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-07-07 09:36:45 +0100

    wip attempting to hack around spaced quoting in custom targets.

--- cmake/BoostLitePrecompiledHeader.cmake
+++ cmake/BoostLitePrecompiledHeader.cmake
@@ -11,9 +11,25 @@ endfunction()
 
 # Adds a custom target which generates a precompiled header
 function(add_precompiled_header outvar headerpath)
+  get_filename_component(header "${headerpath}" NAME)
+  set(pchpath ${CMAKE_CURRENT_BINARY_DIR}/${header}.dir/${CMAKE_CFG_INTDIR}/${header}.pch)
+  set(flags ${CMAKE_CXX_FLAGS}
+
+  )
+  separate_arguments(flags)
+  # FIXME: These generators work, but quote the expansion thus breaking the custom target :(
+  set(flags ${flags}
+#    $<$<CONFIG:Debug>:${CMAKE_CXX_FLAGS_DEBUG}>
+    $<$<CONFIG:Debug>:/A/B/C>
+    $<$<CONFIG:Release>:${CMAKE_CXX_FLAGS_RELEASE}>
+    $<$<CONFIG:RelWithDebInfo>:${CMAKE_CXX_FLAGS_RELWITHDEBINFO}>
+    $<$<CONFIG:MinSizeRel>:${CMAKE_CXX_FLAGS_MINSIZEREL}>
+  )
+  MESSAGE(STATUS "${flags}")
   if(MSVC)
     add_custom_target(${outvar}
-      ${CMAKE_CXX_COMPILER} ${CMAKE_CXX_FLAGS} $<$<CONFIG:Debug>:${CMAKE_CXX_FLAGS_DEBUG}> $<$<CONFIG:Release>:${CMAKE_CXX_FLAGS_RELEASE}> $<$<CONFIG:RelWithDebInfo>:${CMAKE_CXX_FLAGS_RELWITHDEBINFO}> $<$<CONFIG:MinSizeRel>:${CMAKE_CXX_FLAGS_MINSIZEREL}> /Fp"${CMAKE_CURRENT_BINARY_DIR}/${target}.pch" /Yc"${headerpath}"
+      COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_CURRENT_BINARY_DIR}/${header}.dir/${CMAKE_CFG_INTDIR}"
+      COMMAND ${CMAKE_CXX_COMPILER} /c ${flags} /Fp"${pchpath}" /Yc"${header}" /Tp"${CMAKE_CURRENT_SOURCE_DIR}/${headerpath}"
       COMMENT "Precompiling header ${headerpath} ..."
       SOURCES "${headerpath}"
     )
