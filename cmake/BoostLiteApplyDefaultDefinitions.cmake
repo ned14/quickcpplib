415bbcd24580d1a0829464234d6e513e99e2a4fe
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-07-20 08:57:52 +0100

    Libraries need to go into /lib!

--- cmake/BoostLiteApplyDefaultDefinitions.cmake
+++ cmake/BoostLiteApplyDefaultDefinitions.cmake
@@ -100,6 +100,8 @@ endfunction()
 
 # Place all binaries into the bin directory
 all_target_properties(PROPERTIES
+  ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+  LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
   RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
 )
 
