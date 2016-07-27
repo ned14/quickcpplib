62f395a7025d39bee52941afd21326443b5c40f9
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-07-27 07:28:25 +0100

    Moved the ctest scripting boilerplate into BoostLiteUtils.cmake

--- cmake/BoostLiteUtils.cmake
+++ cmake/BoostLiteUtils.cmake
@@ -213,3 +213,48 @@ function(find_boostish_library library version)
     endif()
   endif()
 endfunction()
+
+# Configures a CTest script with a sensible set of defaults
+# for doing a configure, build, test and submission run
+macro(configure_ctest_script_for_cdash projectname bindir)
+  set(CTEST_PROJECT_NAME "${projectname}")
+  set(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+  set(CTEST_BINARY_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${bindir}")
+  set(CTEST_CONFIGURATION_TYPE Release)
+  if(WIN32)
+    set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015 Win64")
+    set(CTEST_CMAKE_CI_BIN_DIR "${bindir}/bin/${CTEST_CONFIGURATION_TYPE}")
+    set(CTEST_SITE $ENV{COMPUTERNAME})
+  else()
+    set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+    set(CTEST_CMAKE_CI_BIN_DIR "${bindir}/bin")
+    set(CTEST_SITE $ENV{NAME})
+  endif()
+  set(CTEST_BUILD_NAME "${CMAKE_SYSTEM}-${CMAKE_SYSTEM_PROCESSOR}")
+
+  find_program(CTEST_PYTHON_COMMAND NAMES python)
+  if(EXISTS "../boost-lite/scripts/merge_junit_results.py")
+    set(CTEST_BOOSTLITE_SCRIPTS "../boost-lite/scripts")
+  elseif(EXISTS "include/boost/outcome/boost-lite/scripts/merge_junit_results.py")
+    set(CTEST_BOOSTLITE_SCRIPTS "include/boost/outcome/boost-lite/scripts")
+  else()
+    message(FATAL_ERROR "FATAL: boost-lite/scripts/merge_junit_results.py not found!")
+  endif()
+endmacro()
+
+function(merge_junit_results_into_ctest_xml)
+  # Merge all the junit XML files from the testing into one junit XML file
+  execute_process(COMMAND "${CTEST_PYTHON_COMMAND}" "${CTEST_BOOSTLITE_SCRIPTS}/merge_junit_results.py" "${CTEST_BINARY_DIRECTORY}/merged_junit_results.xml" "${CTEST_CMAKE_CI_BIN_DIR}/*.junit.xml"
+    RESULT_VARIABLE result
+  )
+  message(STATUS "Merging junit XML results into a single junit XML returned with status ${result}")
+  # Figure out where this iteration's Test.xml lives
+  file(READ "${CTEST_BINARY_DIRECTORY}/Testing/TAG" tag_file)
+  string(REGEX MATCH "[^\n]*" xml_dir "${tag_file}")
+  set(CTEST_XML_DIR "${CTEST_BINARY_DIRECTORY}/Testing/${xml_dir}")
+  # Add the combined junit XML file into our Test.xml
+  execute_process(COMMAND "${CTEST_PYTHON_COMMAND}" "${CTEST_BOOSTLITE_SCRIPTS}/add_junit_results_to_ctest.py" "${CTEST_XML_DIR}/Test.xml" "${CTEST_BINARY_DIRECTORY}/merged_junit_results.xml"
+    RESULT_VARIABLE result
+  )
+  message(STATUS "Merging junit XML results into the CTest XML returned with status ${result}")
+endfunction()
