7f564d466c332923a73b4885edb2b233120719d7
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-05-03 00:26:07 +0100

    Finally got round to writing some Python to restamp the licence boilerplate at the top of my source files. It's now all automated yay!

--- gen_binds/genmap.cpp
+++ gen_binds/genmap.cpp
@@ -1,7 +1,25 @@
-/* genmap.cpp
-Uses libclang to parse a STL header into a local namespace bind
-(C) 2014 Niall Douglas http://www.nedproductions.biz/
+/* Uses libclang to parse a STL header into a local namespace bind
+(C) 2014-2017 Niall Douglas <http://www.nedproductions.biz/> (2 commits)
 Created: Sept 2014
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License in the accompanying file
+Licence.txt or at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file Licence.txt or copy at
+          http://www.boost.org/LICENSE_1_0.txt)
 */
 
 #define LOGGING 0
