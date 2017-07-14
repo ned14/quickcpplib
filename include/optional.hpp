2a31d304f7316267fa61f3c1928657570ce7355c
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-07-14 21:34:00 +0100

    Added in a std optional thunking implementation

---
+++ include/optional.hpp
@@ -0,0 +1,58 @@
+/* optional support
+(C) 2017 Niall Douglas <http://www.nedproductions.biz/> (3 commits)
+File Created: Jul 2017
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License in the accompanying file
+Licence.txt or at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file Licence.txt or copy at
+          http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#ifndef QUICKCPPLIB_OPTIONAL_HPP
+#define QUICKCPPLIB_OPTIONAL_HPP
+
+#include "config.hpp"
+
+#if _HAS_CXX17 || __cplusplus >= 201700
+
+#include <optional>
+
+QUICKCPPLIB_NAMESPACE_BEGIN
+
+namespace optional
+{
+  template <class T> using optional = std::optional<T>;
+}
+
+QUICKCPPLIB_NAMESPACE_END
+
+#else
+
+#include "optional/optional.hpp"
+
+QUICKCPPLIB_NAMESPACE_BEGIN
+
+namespace optional
+{
+  template <class T> using optional = std::experimental::optional<T>;
+}
+
+QUICKCPPLIB_NAMESPACE_END
+
+#endif
+
+#endif
