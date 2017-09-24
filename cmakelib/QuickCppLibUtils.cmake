7a4ec268ad4918a0c7de59dc0e4cb72ed04f3018
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-09-24 15:17:16 +0100

    Definitely fixed appveyor now

--- cmakelib/QuickCppLibUtils.cmake
+++ cmakelib/QuickCppLibUtils.cmake
@@ -386,7 +386,7 @@ macro(CONFIGURE_CTEST_SCRIPT_FOR_CDASH projectname bindir)
   if(WIN32)
     if(NOT DEFINED CTEST_CMAKE_GENERATOR)
       # TODO Figure out how to use winclang via adding in -T v140_clang_c2
-      set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015 Win64")
+      set(CTEST_CMAKE_GENERATOR "Visual Studio 15 2017 Win64")
     endif()
     set(CTEST_CMAKE_CI_BIN_DIR "${bindir}/bin/${CTEST_CONFIGURATION_TYPE}")
     set(CTEST_SITE $ENV{COMPUTERNAME})
