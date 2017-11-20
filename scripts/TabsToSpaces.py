c63d3daf4bae85183c081d378d76a1cff1ca0f79
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-11-20 13:31:55 +0000

    upd

--- scripts/TabsToSpaces.py
+++ scripts/TabsToSpaces.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 spaces_per_tab = 4
 
 import os, sys
 
 for dirpath, dirnames, filenames in os.walk('.'):
 	for filename in filenames:
-		if filename[-4:]=='.hpp' or filename[-4:]=='.cpp' or filename[-4:]=='.ipp':
+		if filename[-4:]=='.hpp' or filename[-4:]=='.cpp' or filename[-4:]=='.ipp' or filename[-4:]=='.txt' or filename[-3:]=='.md':
 			path=os.path.join(dirpath, filename)
 			if os.path.exists(path+'.orig'):
 				os.remove(path+'.orig')
