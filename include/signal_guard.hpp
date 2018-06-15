491e6c78df40667e4f94deac14ef548260d0c368
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2018-06-15 18:23:14 +0100

    Flip doxygen into EXTRACT_ALL

--- include/signal_guard.hpp
+++ include/signal_guard.hpp
@@ -50,6 +50,7 @@ Distributed under the Boost Software License, Version 1.0.
 
 QUICKCPPLIB_NAMESPACE_BEGIN
 
+//! \brief The namespace for signal_guard
 namespace signal_guard
 {
   //! The signals which can be raised
