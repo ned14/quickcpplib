89903ff071cb8d8050530fb78f25ff17cb09c991
Jenkins nedprod CI <foo@nowhere>
2019-05-16 08:45:48 +0000

    Travis CI updates documentation

--- ensure__stores_8hpp.js
+++ ensure__stores_8hpp.js
@@ -1,7 +1,7 @@
 var ensure__stores_8hpp =
 [
     [ "flush_impl_type", "ensure__stores_8hpp.html#a97761b4fd131f7813a6a977f5669ee51", null ],
-    [ "potentially_unknown_jump_ptr", "ensure__stores_8hpp.html#aedef7268e5ebb0ac3561f274359a5372", null ],
+    [ "potentially_unknown_jump_ptr", "ensure__stores_8hpp.html#a55b64002aa3eede9764d5a2f7c2cb1b1", null ],
     [ "memory_flush", "ensure__stores_8hpp.html#a6c99f75c321071186b802a2ac455a363", [
       [ "memory_flush_none", "ensure__stores_8hpp.html#a6c99f75c321071186b802a2ac455a363a6d3af1f11a3e5dbe57de917cdaa9573f", null ],
       [ "memory_flush_retain", "ensure__stores_8hpp.html#a6c99f75c321071186b802a2ac455a363a03cefe1b3f88f9a55e8ee19f52e2cc5c", null ],
@@ -10,8 +10,8 @@ var ensure__stores_8hpp =
     [ "make_flush_impl", "ensure__stores_8hpp.html#ab1e32eceb3e7589a1c7f3cf559e50147", null ],
     [ "flush_impl", "ensure__stores_8hpp.html#ad577c35a8f1c074a019e8bd4a3968b08", null ],
     [ "potentially_unknown_jump", "ensure__stores_8hpp.html#a87b52550d42e9b4a8fe14e08d666cd68", null ],
-    [ "ensure_stores", "ensure__stores_8hpp.html#a187cb146ba97873773d001b1c3d456a0", null ],
-    [ "ensure_stores", "ensure__stores_8hpp.html#a3e7e2e6506bd178b5277243a6e74f370", null ],
+    [ "ensure_stores", "ensure__stores_8hpp.html#af65c5893e3e892061895ce2d4fe70302", null ],
+    [ "ensure_stores", "ensure__stores_8hpp.html#ad47c8d19090eab3b48516e766af5c5a1", null ],
     [ "memory_flush_none", "ensure__stores_8hpp.html#a6dbce1db3d303aa052cb1f02e9889af7", null ],
     [ "memory_flush_retain", "ensure__stores_8hpp.html#ad2c9da17508241e8f0b99eb11d675cfa", null ],
     [ "memory_flush_evict", "ensure__stores_8hpp.html#a23ef8a95147f1b088a016c67b3ce2c55", null ]
