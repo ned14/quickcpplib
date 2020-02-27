7774cace0fd5fad23f79fef117cffb5e98efc222
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2020-02-27 10:41:07 +0000

    Remove use of IN_LIST entirely.

--- cmakelib/QuickCppLibPolicies.cmake
+++ cmakelib/QuickCppLibPolicies.cmake
@@ -1,11 +1,6 @@
 # cmake_minimum_required() resets policies, so this script
 # resets those policies back to what BoostLite needs again
 
-# Enable IN_LIST always
-if(POLICY CMP0057)
-  cmake_policy(SET CMP0057 NEW)
-endif()
-
 # Apply visibility to all objects
 if(POLICY CMP0063)
   cmake_policy(SET CMP0063 NEW)
