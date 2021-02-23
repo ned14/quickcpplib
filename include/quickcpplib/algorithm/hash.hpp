24d92c71ef5d2200f7e42bc47e6dd59ad6d99fe3
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2021-02-23 14:31:10 +0000

    Fix link error in sha256_hash. Add in SpookyHash's verification test suite.

--- include/quickcpplib/algorithm/hash.hpp
+++ include/quickcpplib/algorithm/hash.hpp
@@ -680,15 +680,19 @@ namespace algorithm
       // not have a licence as it was placed into the public domain.
       static constexpr size_t _CHUNK_SIZE = 64;
       static constexpr size_t _TOTAL_LEN_LEN = 8;
-      static constexpr uint32_t _k[] = {
-      0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,  //
-      0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,  //
-      0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,  //
-      0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,  //
-      0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08,  //
-      0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,  //
-      0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2                                                                           //
-      };
+      static const uint32_t *_k() noexcept
+      {
+        static const uint32_t v[] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,  //
+        0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,  //
+        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,  //
+        0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,  //
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08,  //
+        0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,  //
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2                                                                           //
+        };
+        return v;
+      }
       static constexpr inline uint32_t _right_rot(uint32_t value, unsigned int count) { return (value >> count) | (value << (32 - count)); }
 
     public:
@@ -752,7 +756,7 @@ namespace algorithm
           tocopy = bytes;
         }
         memcpy(_chunk + _chunkidx, data, tocopy);
-        if(data!=nullptr)
+        if(data != nullptr)
         {
           _total_len += tocopy;
           data += tocopy;
@@ -776,6 +780,7 @@ namespace algorithm
       QUICKCPPLIB_TREQUIRES(QUICKCPPLIB_TPRED(sizeof(T) == 1), QUICKCPPLIB_TPRED(std::is_trivially_copyable<T>::value))
       void add(const T *data, size_t bytes) noexcept
       {
+        static const uint32_t *const k = _k();
         /*
          * Note 1: All integers (expect indexes) are 32-bit unsigned integers and addition is calculated modulo 2^32.
          * Note 2: For each round, there is one round constant k[i] and one entry in the message schedule array w[i], 0 = i = 63
@@ -817,7 +822,7 @@ namespace algorithm
           {
             const uint32_t s1 = _right_rot(ah[4], 6) ^ _right_rot(ah[4], 11) ^ _right_rot(ah[4], 25);
             const uint32_t ch = (ah[4] & ah[5]) ^ (~ah[4] & ah[6]);
-            const uint32_t temp1 = ah[7] + s1 + ch + _k[i] + w[i];
+            const uint32_t temp1 = ah[7] + s1 + ch + k[i] + w[i];
             const uint32_t s0 = _right_rot(ah[0], 2) ^ _right_rot(ah[0], 13) ^ _right_rot(ah[0], 22);
             const uint32_t maj = (ah[0] & ah[1]) ^ (ah[0] & ah[2]) ^ (ah[1] & ah[2]);
             const uint32_t temp2 = s0 + maj;
