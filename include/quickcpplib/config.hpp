333b2075ae6c3149fd08f67273a4105c3ef6d99d
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-11-01 11:54:12 +0100

    build: Remove dependence on include paths for configs
    Due to the requirement that the sources can be copy pasted into another
    project without _any_ build system support (including properly setup
    include paths), we split the configuration inputs into two separate
    systems. For in-tree usage we supply the configuration options per
    commandline, while for the installed source we write them to a config
    header file. The source tree contains an empty placeholder in order to
    avoid the need for conditional inclusion logic.

--- include/quickcpplib/config.hpp
+++ include/quickcpplib/config.hpp
@@ -30,7 +30,7 @@ Distributed under the Boost Software License, Version 1.0.
 #include "revision.hpp"
 #endif
 
-#include <quickcpplib/detail/config.hpp>
+#include "detail/config.hpp"
 
 #define QUICKCPPLIB_VERSION_GLUE2(a, b) a##b
 #define QUICKCPPLIB_VERSION_GLUE(a, b) QUICKCPPLIB_VERSION_GLUE2(a, b)
